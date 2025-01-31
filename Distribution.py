df03552c666a91554de7e042432da069db99b5dc
Lydia Kila Taban <tabanlydia@gmail.com>
2025-01-31 10:28:54 +0200

    Add Bell-shape and distribution inidcatr file

---
+++ Distribution.py
@@ -0,0 +1,14 @@
+# 100 runs that are reflected in a histogram
+
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(123)
+final_tails = []
+for x in range(1000):
+    tails = [0]
+    for x in range(10):
+        coin = np.random.randint(0,2)
+        tails.append(tails[x] + coin)
+    final_tails.append(tails[-1])
+plt.hist(final_tails, bins = 10)
+plt.show()
