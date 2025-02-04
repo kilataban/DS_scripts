f89f43440e27e8b1d6060bb1e98e32e43f3d0ae2
Lydia Kila Taban <tabanlydia@gmail.com>
2025-02-04 08:35:06 +0200

    Align file names with standard python conventions

---
+++ random_walk_plot.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100):
+    step = random_walk[-1]
+    dice = np.random.randint(1, 7)
+
+    if dice <= 2:
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step += 1
+    else:
+        step += np.random.randint(1, 7)
+
+    random_walk.append(step)
+
+# Plot random_walk
+plt.plot(random_walk)
+plt.show()
