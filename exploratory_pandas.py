90d37098e946fd191f780e5be479ce34960d5534
Lydia Kila Taban <tabanlydia@gmail.com>
2025-03-31 23:10:51 +0200

    Add changes to the notes

--- exploratory_pandas.py
+++ exploratory_pandas.py
@@ -3,3 +3,18 @@ print(df_name.head())
 
 #Explore DataFrame Info
 print(df_name.info())
+
+#Understand the shape of the data
+print(df_name.shape)
+
+#get summary statistics on the data using describe
+#this is good for a numerical overview of numbers
+print(df_name.describe())
+
+#to display the components of a dataframe
+print(df_name.values)
+
+print(df_name.columns)
+
+#to check number indexing of rows
+print(df_name.index)
