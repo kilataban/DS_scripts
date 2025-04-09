0e5181263698899b03741deabe2a38ea6da25d46
Lydia Kila Taban <tabanlydia@gmail.com>
2025-04-09 23:06:00 +0300

    Add asin syntax

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,1 +1,8 @@
 # Subsetting data categoricaly can involve the or operator or | to select rows from multiple categories
+
+
+#use .isin() method to tackle the problem of writing unnecessarily long statements
+
+colors = ["brown", "black", "tan"]
+condition = dogs["color"].isin(colors)
+dogs[condition]
