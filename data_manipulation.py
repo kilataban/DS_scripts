8fc253ae49519375dd5ccb3c347a4c44f0738139
Lydia Kila Taban <tabanlydia@gmail.com>
2025-04-11 21:58:29 +0300

    Add different column manipulations

--- data_manipulation.py
+++ data_manipulation.py
@@ -19,3 +19,7 @@ print(dogs.head())
 bmi_lt_100 = dogs[dogs["bmi"] < 100]
 bmi_lt_100_height = bmi_lt_100.sort_values("height_cm", ascending=False)
 bmi_lt_100_height[["name", "height_cm", "bmi"]]
+
+#It is possible add new columns to data as well
+# These operations are called TRANSFORMING, MUTATING, AND FEATURE ENGINEERING
+
