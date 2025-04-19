fe1cd7d9695b5befc5ea94d28f32adb87ff9a0d8
Lydia Kila Taban <tabanlydia@gmail.com>
2025-04-19 23:03:42 +0300

    This yeilds both the thirtieth and fortieth percentiles

--- summary_statistics.py
+++ summary_statistics.py
@@ -18,3 +18,20 @@ dogs["date_of_birth"].min()
 
 dogs["date_of_birth"].max()
 
+# The .agg() method
+# .agg() does a full aggregation
+def pct30(column):
+    return column.quantile(0.3)
+# this yields the 30th percentile
+
+dogs["weight_kg"].agg(pct30)
+
+# agg can be used on multiple columns
+dogs[["weight_kg", "height _cm"]].agg(pct30)
+
+# MULTIPLE SUMMARIES
+def pct40(column):
+    return column.quantile(0.4)
+
+# this yields the fortieth percentile
+dogs["weight_kg"].agg([pct30, pct40])
