463b6de31ba09bb6c5d6b85562626438ae8268d0
Lydia Kila Taban <tabanlydia@gmail.com>
2025-04-19 22:51:15 +0300

    Add other numerical statistics methods

--- summary_statistics.py
+++ summary_statistics.py
@@ -1,4 +1,11 @@
 # Aggregating data from summary stats
 
+# Summarising numerical data
+
 # 1. Mean
 dogs["height_cm"].mean()
+
+# Other methods of summary include
+.median()  .mode() 
+.min()     .max()
+.var()     .std()
