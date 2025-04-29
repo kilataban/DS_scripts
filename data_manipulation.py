6ac4cfba457f521bff5185760a873b41e6222218
Kila Taban <tabanlydia@gmail.com>
2025-04-29 22:24:15 +0200

    Update data_manipulation.py

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,7 +1,7 @@
 # Subsetting data categoricaly can involve the or operator or | to select rows from multiple categories
 
 
-#use .isin() method to tackle the problem of writing unnecessarily long statements
+# use .isin() method to tackle the problem of writing unnecessarily long statements
 
 colors = ["brown", "black", "tan"]
 condition = dogs["color"].isin(colors)
