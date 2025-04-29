65aa3b0c5f17d0c096376cd7bab512bd1eb3ac35
Kila Taban <tabanlydia@gmail.com>
2025-04-29 22:23:56 +0200

    Update data_manipulation.py

--- data_manipulation.py
+++ data_manipulation.py
@@ -22,7 +22,7 @@ bmi_lt_100_height = bmi_lt_100.sort_values("height_cm", ascending=False)
 
 bmi_lt_100_height[["name", "height_cm", "bmi"]]
 
-#It is possible add new columns to data as well
+# It is possible add new columns to data as well
 # These operations are called TRANSFORMING, MUTATING, AND FEATURE ENGINEERING
 
 # Add total col as sum of individuals and family_members
