9b2d9309498bbeb2782faacf4b8166f1062fd4c4
Kila Taban <tabanlydia@gmail.com>
2025-05-08 23:42:20 +0300

    Update exploratory_pandas.py

--- exploratory_pandas.py
+++ exploratory_pandas.py
@@ -7,11 +7,11 @@ print(df_name.info())
 #Understand the shape of the data
 print(df_name.shape)
 
-#get summary statistics on the data using describe
+#Get summary statistics on the data using describe
 #this is good for a numerical overview of numbers
 print(df_name.describe())
 
-#how to display the components of a dataframe
+#How to display the components of a dataframe
 print(df_name.values)
 
 print(df_name.columns)
